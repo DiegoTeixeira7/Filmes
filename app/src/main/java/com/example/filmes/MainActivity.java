c36024bb97444dbe7d8beb869882f9635e48242b
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2021-05-25 08:15:27 -0300

    add classes

--- app/src/main/java/com/example/filmes/MainActivity.java
+++ app/src/main/java/com/example/filmes/MainActivity.java
@@ -15,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private List<Foto> fotos;
-    private List<String> descricoes;
     private int atual;
     private TextView textView;
     private ImageButton imageButton;
@@ -31,21 +30,13 @@ public class MainActivity extends AppCompatActivity {
     private void inicialize() {
         imageView = findViewById(R.id.imagem);
 
-        descricoes = new ArrayList<>();
-
-        descricoes.add("Em um mundo onde é possível entrar na mente humana, Cobb (Leonardo DiCaprio) está entre os melhores na arte de roubar segredos valiosos do inconsciente, durante o estado de sono. Além disto ele é um fugitivo, pois está impedido de retornar aos Estados Unidos devido à morte de Mal (Marion Cotillard). Desesperado para rever seus filhos, Cobb aceita a ousada missão proposta por Saito (Ken Watanabe), um empresário japonês: entrar na mente de Richard Fischer (Cillian Murphy), o herdeiro de um império econômico, e plantar a ideia de desmembrá-lo.");
-        descricoes.add("Após ver a Terra consumindo boa parte de suas reservas naturais, um grupo de astronautas recebe a missão de verificar possíveis planetas para receberem a população mundial, possibilitando a continuação da espécie. Cooper (Matthew McConaughey) é chamado para liderar o grupo e aceita a missão sabendo que pode nunca mais ver os filhos. Ao lado de Brand (Anne Hathaway), Jenkins (Marlon Sanders) e Doyle (Wes Bentley), ele seguirá em busca de uma nova casa.");
-        descricoes.add("O astronauta Mark Watney (Matt Damon) é enviado a uma missão em Marte. Após uma severa tempestade ele é dado como morto, abandonado pelos colegas e acorda sozinho no misterioso planeta com escassos suprimentos, sem saber como reencontrar os companheiros ou retornar à Terra.");
-        descricoes.add("Em Até o Último Homem, durante a Segunda Guerra Mundial, o médico do exército Desmond T. Doss (Andrew Garfield) se recusa a pegar em uma arma e matar pessoas, porém, durante a Batalha de Okinawa ele trabalha na ala médica e salva mais de 75 homens, sendo condecorado. O que faz de Doss o primeiro Opositor Consciente da história norte-americana a receber a Medalha de Honra do Congresso.");
-        descricoes.add("Em Vingadores: Ultimato, após Thanos eliminar metade das criaturas vivas em Vingadores: Guerra Infinita, os heróis precisam lidar com a dor da perda de amigos e seus entes queridos. Com Tony Stark (Robert Downey Jr.) vagando perdido no espaço sem água nem comida, o Capitão América/Steve Rogers (Chris Evans) e a Viúva Negra/Natasha Romanov (Scarlett Johansson) precisam liderar a resistência contra o titã louco.");
-
         fotos = new ArrayList<>();
 
-        Foto foto1 = new Foto(R.drawable.aorigem, descricoes.get(0));
-        Foto foto2 = new Foto(R.drawable.interestelar, descricoes.get(1));
-        Foto foto3 = new Foto(R.drawable.perdidoemmarte, descricoes.get(2));
-        Foto foto4 = new Foto(R.drawable.ateoultimohomem, descricoes.get(3));
-        Foto foto5 = new Foto(R.drawable.vingadoresultimato, descricoes.get(4));
+        Foto foto1 = new Foto(R.drawable.johnwick, "John Wick (Keanu Reeves) já foi um dos assassinos mais temidos da cidade de Nova York, trabalhando em parceria com a máfia russa. Um dia, ele decide se aposentar, e neste período tem que lidar com a triste morte de sua esposa. Vítima de uma doença grave, ela já previa a sua própria morte, e deu de presente ao marido um cachorro para cuidar em seu período de luto. No entanto, poucos dias após o funeral, o cachorro é morto por ladrões que roubam o seu carro. John Wick parte em busca de vingança contra estes homens que ele já conhecia muito bem.");
+        Foto foto2 = new Foto(R.drawable.jogovorazes, "Num futuro distante, boa parte da população é controlada por um regime totalitário, que relembra esse domínio realizando um evento anual - e mortal - entre os 12 distritos sob sua tutela. Para salvar sua irmã caçula, a jovem Katniss Everdeen (Jennifer Lawrence) se oferece como voluntária para representar seu distrito na competição e acaba contando com a companhia de Peeta Melark (Josh Hutcherson), desafiando não só o sistema dominante, mas também a força dos outros oponentes.");
+        Foto foto3 = new Foto(R.drawable.matrix, "Em um futuro próximo, Thomas Anderson (Keanu Reeves), um jovem programador de computador que mora em um cubículo escuro, é atormentado por estranhos pesadelos nos quais encontra-se conectado por cabos e contra sua vontade, em um imenso sistema de computadores do futuro. Em todas essas ocasiões, acorda gritando no exato momento em que os eletrodos estão para penetrar em seu cérebro. À medida que o sonho se repete, Anderson começa a ter dúvidas sobre a realidade.");
+        Foto foto4 = new Foto(R.drawable.fragmentado, "Kevin (James McAvoy) possui 23 personalidades distintas e consegue alterná-las quimicamente em seu organismo apenas com a força do pensamento. Um dia, ele sequestra três adolescentes que encontra em um estacionamento. Vivendo em cativeiro, elas passam a conhecer as diferentes facetas de Kevin e precisam encontrar algum meio de escapar.");
+        Foto foto5 = new Foto(R.drawable.hp, "Harry Potter (Daniel Radcliffe) é um garoto órfão de 10 anos que vive infeliz com seus tios, os Dursley. Até que, repentinamente, ele recebe uma carta contendo um convite para ingressar em Hogwarts, uma famosa escola especializada em formar jovens bruxos. Inicialmente Harry é impedido de ler a carta por seu tio Válter (Richard Griffiths), mas logo ele recebe a visita de Hagrid (Robbie Coltrane), o guarda-caça de Hogwarts, que chega em sua casa para levá-lo até a escola.");
 
         fotos.add(foto1);
         fotos.add(foto2);
@@ -62,13 +53,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void avance(View view) {
-//        atual = (atual + 1) % fotos.size(); // 0 1 2 3 4
+//        atual = (atual + 1) % fotos.size(); // 0 1 2
 
         if (atual == fotos.size() - 1) {
             atual = 0;
         } else {
             atual++;
-        } // 0 1 2 3 4
+        } // 0 1 2
 
 
         imageView.setImageResource(fotos.get(atual).getId());
@@ -77,12 +68,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void volte(View view) {
-//        atual = (atual < 0) ? 0 : atual - 1; // 4 3 2 1 0
+//        atual = (atual < 0) ? 0 : atual - 1; // 2 1 0
         // atual = pergunta ? verdade : falsidade;
         if (atual == 0) {
-            atual = 4;
+            atual = fotos.size() - 1;
         } else {
-            atual = atual - 1; // atual--;
+            atual--;
         }
 
         imageView.setImageResource(fotos.get(atual).getId());
